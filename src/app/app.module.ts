61e38f4a8b81831ea6f1f3e17b1f5c5fc47149bc
saumya-g <saumyajaipur2012@gmail.com>
2019-10-21 23:07:02 +0530

    Added 10 files for view home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms';
+ 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ActivityComponent } from './activity/activity.component';
 import { BsNavigationComponent } from './bs-navigation/bs-navigation.component';
+import { ArticleComponent } from './article/article.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ActivityComponent,
-    BsNavigationComponent
+    BsNavigationComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/article/article.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+  @Input() title: String;
+  @Input() img: String;
+  @Input() article: String;
+  public likeCounter = 0;
+  public dislikeCounter = 0;
+  constructor() { }
+
+  commentText : String = ""
+  public commentArray = [];
+
+  addComment() {
+    if(this.commentText != ""){
+    this.commentArray.push(this.commentText);
+    this.commentText=""
+    }
+  }
+
+  increaseCounter(type: string) {
+    if (type === "like") {
+      this.likeCounter++;
+    } else {
+      this.dislikeCounter++;
+    }
+
+  }
+
+deleteComment(index: number){
+   this.commentArray.splice(index,1);
+}
+  ngOnInit() {
+  }
+}
