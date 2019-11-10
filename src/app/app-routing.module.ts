49168e4e2a08186a1199993a38626a643c3a9137
SwatiMhatre <swatimhatre16@rediffmail.com>
2019-11-11 02:12:15 +0530

    Authentication guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ActivityComponent } from './activity/activity.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: "", pathMatch: "full", component: WelcomeComponent },
-  { path: "home", component: HomeComponent },
-  { path: "activity", component: ActivityComponent },
+  { path: "home", component: HomeComponent, canActivate: [AuthGuard] },
+  { path: "activity", component: ActivityComponent, canActivate: [AuthGuard] },
   { path: "login", loadChildren: "./auth/auth-module.module#AuthModule" },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
